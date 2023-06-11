aba0014a3d4befb11debe074cbe42d3341ddd351
Maksym Kuzminskii <kymaxgen@gmail.com>
2023-06-11 23:05:12 +0300

    improove hometask Rugby08

--- Rugby08.java
+++ Rugby08.java
@@ -1,25 +1,32 @@
 package gmail.kymaxgen.Hillel_school;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Rugby08 {
     public static void main(String[] args) {
         int[] firstTeamAges = new int[25];
         int[] secondTeamAges = new int[25];
-        System.out.print("У першій команді грають істоти такого віку: ");
-        fillShowPrintAverage(firstTeamAges);
-        System.out.print("Друга команда має гравців такого віку: ");
-        fillShowPrintAverage(secondTeamAges);
+        fillArray(firstTeamAges);
+        fillArray(secondTeamAges);
+        System.out.print("У першій команді грають істоти такого віку: " + Arrays.toString(firstTeamAges));
+        System.out.printf("%nСередній вік гравців першої команди: %d;%n", countAverage(firstTeamAges));
+        System.out.print("Друга команда має гравців такого віку: " + Arrays.toString(secondTeamAges));
+        System.out.printf("%nСередній вік гравців другої команди: %d;%n", countAverage(secondTeamAges));
     }
 
-    public static void fillShowPrintAverage(int[] array) {
+    public static void fillArray(int[] array) {
         Random random = new Random();
-        int TeamTotalAge = 0;
-        for (int i : array) {
-            i = random.nextInt(18, 40);
-            System.out.print(i + ", ");
-            TeamTotalAge += i;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(18, 40);
         }
-        System.out.printf("%nСередній вік гравців цієї команди: %d;%n", Math.round((float) TeamTotalAge / 25));
+    }
+
+    public static int countAverage(int[] array) {
+        int teamTotalAge = 0;
+        for (int i : array) {
+            teamTotalAge += i;
+        }
+        return teamTotalAge / 25;
     }
 }
